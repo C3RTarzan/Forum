bac810fb86abbb7ccb332c8702a09ecef0ad1d51
C3RTarzan <allanhivdc@gmail.com>
2021-11-15 18:49:01 -0300

    Front static

---
+++ static/header.php
@@ -0,0 +1,67 @@
+<link rel="stylesheet" href="./public/css/header.css">
+<header>
+    <div class="logo flex">
+        <span>Logo</span>
+    </div>
+    <div class="search flex">
+        <form action="#" method="get">
+            <input class="search-text" type="text" placeholder="Buscar">
+            <button><span class="iconify" data-icon="ic:round-search"></span></button>
+        </form>
+    </div>
+    <div class="option flex">
+        <div class="option-grid">
+            <div class="user icons flex">
+                <div class="icon-user icon flex">
+                    <span class="iconify" data-icon="carbon:user-avatar-filled-alt"></span>
+                </div>
+                <div class="menu-user menu">
+                    <div class="account iten">
+                        <span class="iconify" data-icon="line-md:account"></span>
+                        <span class="text">Conta</span>
+                    </div>
+                    <div class="config iten">
+                        <span class="iconify" data-icon="icon-park-outline:config"></span>
+                        <span class="text">Configuração</span>
+                    </div>
+                    <div class="exit iten">
+                        <span class="iconify" data-icon="cil:account-logout"></span>
+                        <span class="text">Sair</span>
+                    </div>
+                </div>
+            </div>
+            <div class="notification icons flex">
+                <div class="icon-notification icon flex">
+                    <span class="iconify" data-icon="clarity:notification-outline-badged"></span>
+                </div>
+                <div class="menu-notification menu">
+                    <div class="iten">
+                        <span class="text open">Usuario Zé curtiu seu post</span>
+                    </div>
+                    <div class="iten">
+                        <span class="text">Usuario Zé curtiu seu post</span>
+                    </div>
+                    <div class="iten">
+                        <span class="text">Usuario Zé curtiu seu post</span>
+                    </div>
+                </div>
+            </div>
+            <div class="config icons flex">
+                <div class="icon-config icon flex">
+                    <span class="iconify" data-icon="ph:gear-six-light"></span>
+                </div>
+                <div class="menu-config menu">
+                    <div class="iten">
+                        <span class="iconify" data-icon="ri:home-gear-fill"></span>
+                        <span class="text">Configuração</span>
+                    </div>
+                    <div class="iten">
+                        <!--<span class="iconify" data-icon="emojione-monotone:waxing-crescent-moon"></span>-->
+                        <span class="iconify" data-icon="emojione-monotone:waning-gibbous-moon"></span>
+                        <span class="text">Dark Mode</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ static/menu.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="./public/css/main.css">
+<div class="margin">
+
+</div>
+<section id="menu">
+    <div class="button-menu">
+        <span>Início</span>
+    </div>
+    <div class="itens-menu">
+        <span>Perguntas</span>
+        <span>Tags</span>
+        <span>Usuários</span>
+        <span>Ranking</span>
+    </div>
+    <div class="button-menu">
+        <span>Sobre</span>
+    </div>
+    <div class="itens-menu">
+        <span>Dúvidas</span>
+        <span>Tutoriais</span>
+    </div>
+</section>
